5bde536863c91e8c3cfa5966d4ece19c1f9c9d71
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-03-11 10:17:15 +0100

    Solved

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,17 +4,24 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+const sliders = player.querySelectorAll('.player__slider');
 
 function togglePlay() {
     if (video.paused) {
         video.play();
-        toggle.innerHTML = "❚❚";
     }
     else {
         video.pause();
+    }
+}
+
+function togglePlayButton() {
+    if (video.paused) {
         toggle.innerHTML = "►";
-    };
+    }
+    else {
+        toggle.innerHTML = "❚❚";
+    }
 }
 
 function updateProgress() {
@@ -22,7 +29,37 @@ function updateProgress() {
     progressBar.style.flexBasis = `${percent}%`;
 }
 
-toggle.addEventListener("click", togglePlay);
-video.addEventListener('timeupdate', updateProgress);
+function updateSlider() {
+    if (this.name == "volume") {
+        video.volume = this.value;
+    }
+    else {
+        video.playbackRate = this.value;
+    }
+}
 
+function skip() {
+        video.currentTime = video.currentTime + parseFloat(this.dataset.skip);    
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
+
+let mousedown = false;
+
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", togglePlayButton);
+video.addEventListener("pause", togglePlayButton);
+video.addEventListener("timeupdate", updateProgress);
+sliders.forEach(slider => slider.addEventListener("change", updateSlider));
+skipButtons.forEach(skipButton => skipButton.addEventListener("click", skip));
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
+
+percent = 0;
 updateProgress();
